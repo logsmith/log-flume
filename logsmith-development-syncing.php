f41694df87e64b77eb392807e0ff580883f1e7a2
David Darke <david@atomicsmash.co.uk>
2017-03-16 23:06:18 +0000

    Added similar options example

--- logsmith-development-syncing.php
+++ logsmith-development-syncing.php
@@ -35,7 +35,6 @@ class DevelopmentSyncing {
     function __construct() {
         add_action( 'load-upload.php', array($this, 'indexButton'));
         add_action( 'admin_menu', array($this, 'submenu') );
-        add_action( 'admin_init', array($this, 'wporg_settings_init') );
 
     }
 
@@ -49,33 +48,6 @@ class DevelopmentSyncing {
         return add_query_arg( array('page'=>'my-custom-upload'), admin_url('upload.php') );
     }
 
-    function wporg_settings_init() {
-        // register a new setting for "wporg" page
-        register_setting( 'wporg', 'wporg_options' );
-
-        // register a new section in the "wporg" page
-        add_settings_section(
-            'wporg_section_developers',
-            __( 'The Matrix has you.', 'wporg' ),
-            'wporg_section_developers_cb',
-            'wporg'
-        );
-
-        // register a new field in the "wporg_section_developers" section, inside the "wporg" page
-        add_settings_field(
-            'wporg_field_pill', // as of WP 4.6 this value is used only internally
-            // use $args' label_for to populate the id inside the callback
-            __( 'Pill', 'wporg' ),
-            'wporg_field_pill_cb',
-            'wporg',
-            'wporg_section_developers',
-            [
-                'label_for' => 'wporg_field_pill',
-                'class' => 'wporg_row',
-                'wporg_custom_data' => 'custom',
-            ]
-        );
-    }
 
     function submenu() {
         add_media_page( self::getButtonLabel(), self::getButtonLabel(), 'upload_files', 'my-custom-upload', array($this, 'admin_page') );
@@ -117,30 +89,23 @@ class DevelopmentSyncing {
 
         // check if the user have submitted the settings
         // wordpress will add the "settings-updated" $_GET parameter to the url
-        if ( isset( $_GET['settings-updated'] ) ) {
-        // add settings saved message with the class of "updated"
-        add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
-        }
-
         // show error/update messages
-        settings_errors( 'wporg_messages' );
         ?>
-        <div class="wrap">
-        <form action="options.php" method="post">
-        <?php
-        // output security fields for the registered setting "wporg"
-        settings_fields( 'wporg' );
-        // output setting sections and their fields
-        // (sections are registered for "wporg", each field is registered to a specific section)
-        do_settings_sections( 'wporg' );
-        // output save settings button
-        submit_button( 'Save Settings' );
-        ?>
-        </form>
-        </div>
+
         <?php
 
-
+        ?>
+    	    <div class="wrap">
+    	    <h1>Theme Panel</h1>
+    	    <form method="post" action="options.php">
+    	        <?php
+    	            settings_fields("section");
+    	            do_settings_sections("theme-options");
+    	            submit_button();
+    	        ?>
+    	    </form>
+    		</div>
+    	<?php
         if(isset($_GET['sync'])){
 
             // define('AWS_ACCESS_KEY_ID','');
@@ -385,35 +350,43 @@ function wporg_section_developers_cb( $args ) {
  <?php
 }
 
-// pill field cb
 
-// field callbacks can accept an $args parameter, which is an array.
-// $args is defined at the add_settings_field() function.
-// wordpress has magic interaction with the following keys: label_for, class.
-// the "label_for" key value is used for the "for" attribute of the <label>.
-// the "class" key value is used for the "class" attribute of the <tr> containing the field.
-// you can add custom key value pairs to be used inside your callbacks.
-function wporg_field_pill_cb( $args ) {
-    // get the value of the setting we've registered with register_setting()
-    $options = get_option( 'wporg_options' );
-    // output the field
-    ?>
-    <select id="<?php echo esc_attr( $args['label_for'] ); ?>"
-    data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
-    name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]"
-    >
-    <option value="red" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'red', false ) ) : ( '' ); ?>>
-    <?php esc_html_e( 'red pill', 'wporg' ); ?>
-    </option>
-    <option value="blue" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'blue', false ) ) : ( '' ); ?>>
-    <?php esc_html_e( 'blue pill', 'wporg' ); ?>
-    </option>
-    </select>
-    <p class="description">
-    <?php esc_html_e( 'You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'wporg' ); ?>
-    </p>
-    <p class="description">
-    <?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'wporg' ); ?>
-    </p>
-    <?php
+
+
+
+
+    function display_twitter_element()
+    {
+    	?>
+        	<input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
+        <?php
     }
+
+    function display_facebook_element()
+    {
+    	?>
+        	<input type="text" name="facebook_url" id="facebook_url" value="<?php echo get_option('facebook_url'); ?>" />
+        <?php
+    }
+
+    function display_layout_element()
+    {
+    	?>
+    		<input type="checkbox" name="theme_layout" value="1" <?php checked(1, get_option('theme_layout'), true); ?> />
+    	<?php
+    }
+
+    function display_theme_panel_fields()
+    {
+    	add_settings_section("section", "All Settings", null, "theme-options");
+
+    	add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
+        add_settings_field("facebook_url", "Facebook Profile Url", "display_facebook_element", "theme-options", "section");
+        add_settings_field("theme_layout", "Do you want the layout to be responsive?", "display_layout_element", "theme-options", "section");
+
+        register_setting("section", "twitter_url");
+        register_setting("section", "facebook_url");
+        register_setting("section", "theme_layout");
+    }
+
+    add_action("admin_init", "display_theme_panel_fields");
